d79bf3a22145f31cad957a55dc8e1fa8cc7373b6
Sebastian <jsruiz7411@misena.edu.co>
2021-02-18 15:56:41 -0500

    Agregar falta completea

--- src/app/components/tareas/tareas.component.ts
+++ src/app/components/tareas/tareas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Tarea } from 'src/app/models/Tarea';
 
 @Component({
   selector: 'app-tareas',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TareasComponent implements OnInit {
 
+  listTareas: Tarea[] = [];
+  nombreTarea = '';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  agregarTarea(){
+
+    
   }
 
 }
